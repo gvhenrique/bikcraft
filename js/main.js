315b5b97d12352dccf883ad0649c75c062ef72f2
Gustavo Henrique <igumartins.2001@gmail.com>
2022-10-27 18:11:52 -0300

    created the animation script and applied the index introduction

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-// Mobile Menu
+// Imports
 import MobileMenu from './modules/mobile-menu';
+import Animation from './modules/animation';
+
+// Mobile Menu
 
 const menuMobile = new MobileMenu('.header__menu', '.header__menu--mobile');
 menuMobile.init();
+
+// Animation
+
+const animation = new Animation();
+animation.init();
